2b4f3b1eceeb2f3860a44ab4a2ff19e022acd82c
VijithWindows <vijith.messi@gmail.com>
2020-06-04 15:03:25 +0530

    probably the last commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -22,11 +22,6 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model=Post       
 
-class CreatePostView(LoginRequiredMixin,CreateView):
-    login_url='/login/'
-    redirect_field_name='blog/post_detail.html'
-    form_class=PostForm
-    model=Post
 
 class PostUpdateView(LoginRequiredMixin,UpdateView):
     login_url='/login/'
@@ -47,37 +42,53 @@ class DraftListView(LoginRequiredMixin,ListView):
 
 ######################################################################
 ######################################################################
-
 @login_required
-def add_comment_to_post(request,pk):
-    post=get_object_or_404(Post,pk=pk)
-    if request.method == 'POST':
-        form=CommentForm(request.POST)
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
         if form.is_valid():
-            comment=form.save(commit=False)
-            comment.post=post
+            comment = form.save(commit=False)
+            comment.post = post
             comment.save()
-            return redirect('post_detail',pk=post.pk)
-        else:
-            form=CommentForm()
-        return render(request,'blog/comment_form.html',{form:form})
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
 
 @login_required
 def comment_approve(request,pk):
     comment=get_object_or_404(Comment,pk=pk)
     comment.approve()
-    return redirect('post_detail'pk=comment.post.pk)
+    return redirect('post_detail',pk=comment.post.pk)
 
 @login_required
 def comment_remove(request,pk):
     comment=get_object_or_404(Comment,pk=pk)
     post_pk=comment.post.pk
     comment.delete()
-    return redirect('post_detail' pk=post_pk)
+    return redirect('post_detail',pk=post_pk)
 
 @login_required
-def post_publish(request,pk):
-    post=get_object_or_404(Post,pk=pk)
-    post.publish
-    return redirect('post_detail',pk=pk)         
+def createPost(request):
+
+    form=PostForm()
+    if request.method=='POST':
+        form=PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    context={'form':form}
+
+    return render(request,'blog/post_form.html',context)
+
+
 
